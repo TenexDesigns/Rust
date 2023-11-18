fb40e5ec4910851e27011b755155f60474012738
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-18 11:59:05 +0300

    Create Binary.java

---
+++ Binary.java
@@ -0,0 +1,23 @@
+Binary data is represented as a series of bytes,  with each byte conayinig 8 bits
+
+  a bit is the individual 1 and 0
+
+
+  And a byte can be represnted as an integer using the binary number system
+
+
+
+  Character encoding
+  -Mapping byte to charcters
+  There is two common encoddings , Ascii and UTF-8
+
+  Ascii has 256 chacter with rach character being represnted by one byte
+  Ascii is very limited as a single byte can represent 256 values
+
+
+
+  UTF-8
+
+  Is a variable with encode coding where characters are are anywhere from one to four bytes, thus utf-8 can encode over a million charcters including evrry charcter in the world and comples emojis, utf-8 is completetly compartiplbe with acii
+
+  Utf- 8 is the most wildy adopted character encoding in the world
