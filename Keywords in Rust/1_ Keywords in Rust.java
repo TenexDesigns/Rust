f1120f179726f8d05e69ad86abdc80d236fe320e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-26 18:17:52 +0300

    Update 1_ Keywords in Rust.java

--- Keywords in Rust/1_ Keywords in Rust.java
+++ Keywords in Rust/1_ Keywords in Rust.java
@@ -1,3 +1,94 @@
+In Rust, keywords are reserved words that have special meanings and cannot be used as identifiers (except as raw identifiers). Identifiers are names of functions, variables, parameters, struct fields, modules, crates, constants, macros, static values, attributes, types, traits, or lifetimes. Here are some of the keywords in Rust and their uses:
+
+**Keywords Currently in Use**
+
+- `as`: Perform primitive casting, disambiguate the specific trait containing an item, or rename items in use statements.
+- `async`: Return a Future instead of blocking the current thread.
+- `await`: Suspend execution until the result of a Future is ready.
+- `break`: Exit a loop immediately.
+- `const`: Define constant items or constant raw pointers.
+- `continue`: Continue to the next loop iteration.
+- `crate`: In a module path, refers to the crate root.
+- `dyn`: Dynamic dispatch to a trait object.
+- `else`: Fallback for if and if let control flow constructs.
+- `enum`: Define an enumeration.
+- `extern`: Link an external function or variable.
+- `false`: Boolean false literal.
+- `fn`: Define a function or the function pointer type.
+- `for`: Loop over items from an iterator, implement a trait, or specify a higher-ranked lifetime.
+- `if`: Branch based on the result of a conditional expression.
+- `impl`: Implement inherent or trait functionality.
+- `in`: Part of for loop syntax.
+- `let`: Bind a variable.
+- `loop`: Loop unconditionally.
+- `match`: Match a value to patterns.
+- `mod`: Define a module.
+- `move`: Make a closure take ownership of all its captures.
+- `mut`: Denote mutability in references, raw pointers, or pattern bindings.
+- `pub`: Denote public visibility in struct fields, impl blocks, or modules.
+- `ref`: Bind by reference.
+- `return`: Return from function.
+- `Self`: A type alias for the type we are defining or implementing.
+- `self`: Method subject or current module.
+- `static`: Global variable or lifetime lasting the entire program execution.
+- `struct`: Define a structure.
+- `super`: Parent module of the current module.
+- `trait`: Define a trait.
+- `true`: Boolean true literal.
+- `type`: Define a type alias or associated type.
+- `union`: Define a union; is only a keyword when used in a union declaration.
+- `unsafe`: Denote unsafe code, functions, traits, or implementations.
+- `use`: Bring symbols into scope.
+- `where`: Denote clauses that constrain a type.
+- `while`: Loop conditionally based on the result of an expression [Source 1](https://doc.rust-lang.org/book/appendix-01-keywords.html).
+
+**Keywords Reserved for Future Use**
+
+The following keywords do not yet have any functionality but are reserved by Rust for potential future use:
+
+- `abstract`
+- `become`
+- `box`
+- `do`
+- `final`
+- `macro`
+- `override`
+- `priv`
+- `try`
+- `typeof`
+- `unsized`
+- `virtual`
+- `yield` [Source 1](https://doc.rust-lang.org/book/appendix-01-keywords.html).
+
+**Raw Identifiers**
+
+Raw identifiers are the syntax that lets you use keywords where they wouldn’t normally be allowed. You use a raw identifier by prefixing a keyword with `r#`. For example, `match` is a keyword. If you try to compile a function that uses `match` as its name, you'll get an error. To use `match` as a function name, you need to use the raw identifier syntax, like this:
+
+```rust
+fn r#match(needle: &str, haystack: &str) -> bool {
+   haystack.contains(needle)
+}
+
+fn main() {
+   assert!(r#match("foo", "foobar"));
+}
+```
+
+This code will compile without any errors. Note the `r#` prefix on the function name in its definition as well as where the function is called in `main`. Raw identifiers allow you to use any word you choose as an identifier, even if that word happens to be a reserved keyword [Source 1](https://doc.rust-lang.org/book/appendix-01-keywords.html).
+
+                                                                                                                                                                                                                                                                                               
+                                                                                                                                                                                                                                                                                               
+                                                                                                                                                                                                                                                                                               
+
+
+
+
+
+
+
+
+=======================================================================================================================================
+
 **Keywords in Rust and Their Uses**
 
 **Keywords Currently in Use:**
