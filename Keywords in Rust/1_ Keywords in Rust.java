58069b787c31c5f2a020c0e984955d77acf194af
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-26 18:14:55 +0300

    Create 1_ Keywords in Rust.java

---
+++ Keywords in Rust/1_ Keywords in Rust.java
@@ -0,0 +1,150 @@
+**Keywords in Rust and Their Uses**
+
+**Keywords Currently in Use:**
+
+1. **`as`:**
+   - **Functionality:** Perform primitive casting, disambiguate specific traits, or rename items in use statements.
+
+2. **`async`:**
+   - **Functionality:** Return a Future instead of blocking the current thread.
+
+3. **`await`:**
+   - **Functionality:** Suspend execution until the result of a Future is ready.
+
+4. **`break`:**
+   - **Functionality:** Exit a loop immediately.
+
+5. **`const`:**
+   - **Functionality:** Define constant items or constant raw pointers.
+
+6. **`continue`:**
+   - **Functionality:** Continue to the next loop iteration.
+
+7. **`crate`:**
+   - **Functionality:** In a module path, refers to the crate root.
+
+8. **`dyn`:**
+   - **Functionality:** Dynamic dispatch to a trait object.
+
+9. **`else`:**
+   - **Functionality:** Fallback for if and if let control flow constructs.
+
+10. **`enum`:**
+   - **Functionality:** Define an enumeration.
+
+11. **`extern`:**
+   - **Functionality:** Link an external function or variable.
+
+12. **`false`:**
+   - **Functionality:** Boolean false literal.
+
+13. **`fn`:**
+   - **Functionality:** Define a function or the function pointer type.
+
+14. **`for`:**
+   - **Functionality:** Loop over items from an iterator, implement a trait, or specify a higher-ranked lifetime.
+
+15. **`if`:**
+   - **Functionality:** Branch based on the result of a conditional expression.
+
+16. **`impl`:**
+   - **Functionality:** Implement inherent or trait functionality.
+
+17. **`in`:**
+   - **Functionality:** Part of for loop syntax.
+
+18. **`let`:**
+   - **Functionality:** Bind a variable.
+
+19. **`loop`:**
+   - **Functionality:** Loop unconditionally.
+
+20. **`match`:**
+   - **Functionality:** Match a value to patterns.
+
+21. **`mod`:**
+   - **Functionality:** Define a module.
+
+22. **`move`:**
+   - **Functionality:** Make a closure take ownership of all its captures.
+
+23. **`mut`:**
+   - **Functionality:** Denote mutability in references, raw pointers, or pattern bindings.
+
+24. **`pub`:**
+   - **Functionality:** Denote public visibility in struct fields, impl blocks, or modules.
+
+25. **`ref`:**
+   - **Functionality:** Bind by reference.
+
+26. **`return`:**
+   - **Functionality:** Return from a function.
+
+27. **`Self`:**
+   - **Functionality:** Type alias for the type being defined or implemented.
+
+28. **`self`:**
+   - **Functionality:** Method subject or current module.
+
+29. **`static`:**
+   - **Functionality:** Global variable or lifetime lasting the entire program execution.
+
+30. **`struct`:**
+   - **Functionality:** Define a structure.
+
+31. **`super`:**
+   - **Functionality:** Parent module of the current module.
+
+32. **`trait`:**
+   - **Functionality:** Define a trait.
+
+33. **`true`:**
+   - **Functionality:** Boolean true literal.
+
+34. **`type`:**
+   - **Functionality:** Define a type alias or associated type.
+
+35. **`union`:**
+   - **Functionality:** Define a union (only a keyword when used in a union declaration).
+
+36. **`unsafe`:**
+   - **Functionality:** Denote unsafe code, functions, traits, or implementations.
+
+37. **`use`:**
+   - **Functionality:** Bring symbols into scope.
+
+38. **`where`:**
+   - **Functionality:** Denote clauses that constrain a type.
+
+39. **`while`:**
+   - **Functionality:** Loop conditionally based on the result of an expression.
+
+**Keywords Reserved for Future Use:**
+
+- `abstract`
+- `become`
+- `box`
+- `do`
+- `final`
+- `macro`
+- `override`
+- `priv`
+- `try`
+- `typeof`
+- `unsized`
+- `virtual`
+- `yield`
+
+**Raw Identifiers:**
+
+- Raw identifiers are used when you want to use keywords as identifiers.
+- Prefix a keyword with `r#` to indicate it is a raw identifier.
+- Example: `r#match` can be used as an identifier instead of the reserved keyword `match`.
+
+
+
+
+
+
+
+  
