33623da5146efd12d2cebc6ceed13b72e54e9bff
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-28 10:50:12 +0300

    Create 2_Operators.java

---
+++ Operators in Rust/2_Operators.java
@@ -0,0 +1,63 @@
+Sure, here is a more detailed explanation of some of the operators in the table:
+
+**Arithmetic Operators**
+
+Arithmetic operators are used to perform mathematical operations on numbers. Rust supports the following arithmetic operators:
+
+* **Addition (+)**: Adds two numbers together.
+* **Subtraction (-)**: Subtracts one number from another.
+* **Multiplication (*)**: Multiplies two numbers together.
+* **Division (/)**: Divides one number by another.
+* **Modulo (%)**: Returns the remainder after dividing one number by another.
+
+**Bitwise Operators**
+
+Bitwise operators are used to perform operations on individual bits of binary numbers. Rust supports the following bitwise operators:
+
+* **Bitwise AND (&)**: Performs a logical AND operation on each bit of two binary numbers.
+* **Bitwise OR (|)**: Performs a logical OR operation on each bit of two binary numbers.
+* **Bitwise XOR (^)**: Performs a logical XOR operation on each bit of two binary numbers.
+* **Bitwise NOT (~)**: Inverts each bit of a binary number.
+* **Left shift (<<)**: Shifts the bits of a binary number to the left.
+* **Right shift (>>)**: Shifts the bits of a binary number to the right.
+
+**Comparison Operators**
+
+Comparison operators are used to compare two values. Rust supports the following comparison operators:
+
+* **Equality (==)**: Checks if two values are equal.
+* **Inequality (!=)**: Checks if two values are not equal.
+* **Less than (<)**: Checks if one value is less than another.
+* **Greater than (>)**: Checks if one value is greater than another.
+* **Less than or equal to (<=)**: Checks if one value is less than or equal to another.
+* **Greater than or equal to (>=)**: Checks if one value is greater than or equal to another.
+
+**Logical Operators**
+
+Logical operators are used to combine multiple boolean expressions. Rust supports the following logical operators:
+
+* **Logical AND (&&)**: Returns true if both operands are true, and false otherwise.
+* **Logical OR (||)**: Returns true if either operand is true, and false otherwise.
+* **Logical NOT (!)**: Inverts the value of a boolean expression.
+
+**Assignment Operators**
+
+Assignment operators are used to assign values to variables. Rust supports the following assignment operators:
+
+* **Simple assignment (=)**: Assigns a value to a variable.
+* **Compound assignment operators (+=, -=, *=, /=, %=, &=, |=, ^=, <<=, >>=)**: Combine an assignment operation with another arithmetic or bitwise operation.
+
+**Other Operators**
+
+Rust also supports a few other operators, including:
+
+* **Unary negation (-)**: Inverts the sign of a number.
+* **Member access (.)**: Accesses a member of a struct or enum.
+* **Range operator (..)**: Creates a range of numbers.
+* **Dereference operator (*)**: Dereferences a pointer or reference.
+* **Pattern matching operator (@)**: Used in pattern matching expressions.
+
+I hope this provides a more comprehensive understanding of the operators in Rust. Please let me know if you have any further questions.
+
+
+  
