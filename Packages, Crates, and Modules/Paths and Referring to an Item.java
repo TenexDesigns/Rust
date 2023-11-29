03861303921ebd79a26c5ef69bc92f34e6714a0a
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-29 11:33:20 +0300

    Create Paths and Referring to an Item.java

---
+++ Packages, Crates, and Modules/Paths and Referring to an Item.java
@@ -0,0 +1,131 @@
+In Rust, the `mod` keyword is used to define a module, and the `pub` keyword is used to make items public. By default, all items (functions, methods, structs, enums, modules, and constants) are private. They can only be used by the current module and its descendants. If you want to allow external code to access and use an item, you need to make it public with `pub`.
+
+Here's an example of defining modules and making them public:
+
+```rust
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+```
+
+In this example, `front_of_house` is a module that contains another module `hosting`, which has a function `add_to_waitlist`. The `pub` keyword makes the `hosting` module and the `add_to_waitlist` function public, meaning they can be accessed from outside the `front_of_house` module.
+
+You can also use `pub` with structs and enums. However, there are some differences:
+
+- If you use `pub` before a struct definition, the struct becomes public, but its fields remain private. You can make each field public or not on a case-by-case basis.
+
+```rust
+pub struct Breakfast {
+    pub toast: String,
+    seasonal_fruit: String,
+}
+```
+
+In this example, the `Breakfast` struct and its `toast` field are public, but the `seasonal_fruit` field is private.
+
+- If you use `pub` before an enum definition, the enum becomes public, and all of its variants are also public.
+
+```rust
+pub enum Appetizer {
+    Soup,
+    Salad,
+}
+```
+
+In this example, the `Appetizer` enum and its variants (`Soup` and `Salad`) are public.
+
+To access items in a module, you can use paths. A path can either be absolute or relative. Absolute paths start from the crate root by using `crate`. Relative paths start from the current module and use `self`, `super`, or an identifier in the current module.
+
+```rust
+// Absolute path
+crate::front_of_house::hosting::add_to_waitlist();
+
+// Relative path
+front_of_house::hosting::add_to_waitlist();
+```
+
+In this example, the `add_to_waitlist` function is accessed using both an absolute path and a relative path.
+
+The `use` keyword can be used to bring a path into scope, making it easier to use.
+
+```rust
+use crate::front_of_house::hosting;
+
+fn main() {
+    hosting::add_to_waitlist();
+}
+```
+
+In this example, the `use` keyword brings the `hosting` module into scope, so `add_to_waitlist` can be called more concisely.
+
+As your project grows, you can separate modules into different files to make your code more manageable. Each file will contain the module's contents, and you can use the `mod` keyword to bring the module into scope.
+
+```rust
+// In src/front_of_house.rs
+pub mod hosting {
+    pub fn add_to_waitlist() {}
+}
+
+// In src/main.rs
+mod front_of_house;
+
+use crate::front_of_house::hosting;
+
+fn main() {
+    hosting::add_to_waitlist();
+}
+```
+
+In this example, the `front_of_house` module is defined in a separate file `front_of_house.rs`, and it's brought into scope in `main.rs` using the `mod` keyword.
+
+Key takeaways and best practices:
+
+- Use modules to organize your code and control the privacy of items.
+- By default, all items are private. Use the `pub` keyword to make them public.
+- Use absolute paths or relative paths to access items in a module.
+- Use the `use` keyword to bring a path into scope and make your code more concise.
+- As your project grows, separate modules into different files to make your code more manageable.
+- Be mindful of the privacy rules when organizing your code. Make sure that items are public if they need to be accessed from outside their current module.
+
+  
+
+
+
+
+
+
+
+-----------------------------------------------------------------
+The provided text covers several important concepts related to the Rust module system. Here are the key takeaways and best practices:
+
+1. **Paths for Referring to Items:**
+   - Absolute paths start from the crate root, and relative paths start from the current module.
+   - Paths can use `self`, `super`, or identifiers to navigate through the module tree.
+   - Both absolute and relative paths use double colons (`::`) to separate identifiers.
+
+2. **Private by Default:**
+   - In Rust, all items (functions, methods, structs, enums, modules, and constants) are private to their parent modules by default.
+   - Modules provide encapsulation, and their contents are private unless explicitly marked as public.
+
+3. **Exposing Paths with the `pub` Keyword:**
+   - The `pub` keyword is used to make modules and their contents public.
+   - Making a module public allows code in ancestor modules to refer to it, but it doesn't automatically make the contents of the module public.
+   - To expose items within a module, the `pub` keyword must be used for each item.
+
+4. **Best Practices for Packages with a Binary and a Library:**
+   - It's common for a Rust package to contain both a binary crate (in `src/main.rs`) and a library crate (in `src/lib.rs`).
+   - The module tree and public API are typically defined in `src/lib.rs`, and the binary crate acts as a user of the library crate.
+   - This organization helps in designing a clear API for users and maintaining a separation between internal implementation details and the public interface.
+
+5. **Starting Relative Paths with `super`:**
+   - Relative paths can begin in the parent module by using `super` at the start of the path.
+   - This is useful when modules are closely related, and using `super` can simplify updates if modules are moved within the module tree.
+
+6. **Making Structs and Enums Public:**
+   - The `pub` keyword can be used to make structs and enums public.
+   - For structs, individual fields need to be marked as public if they are intended to be accessible from outside the module.
+   - Enums are typically fully public, and their variants are accessible without additional annotations.
+
+Overall, the `pub` keyword plays a crucial role in controlling visibility and exposing the desired parts of a module's API to external code. Understanding and carefully using visibility modifiers are essential for creating maintainable and well-designed Rust code.
