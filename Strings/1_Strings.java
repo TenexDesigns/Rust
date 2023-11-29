9b40e75962012dab474bda45334923eae14034ba
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-29 14:23:29 +0300

    Create 1_Strings.java

---
+++ Strings/1_Strings.java
@@ -0,0 +1,207 @@
+In Rust, strings are a bit more complex than in many other languages due to their handling of Unicode. Rust has two main types for strings: `String` and `&str`.
+
+`String` is a growable, mutable, owned, UTF-8 encoded string type. It's provided by Rust's standard library. Here's how you can create a new `String`:
+
+```rust
+let mut s = String::new();
+```
+
+You can also create a `String` from a string literal:
+
+```rust
+let s = "initial contents".to_string();
+```
+
+Or using the `String::from` function:
+
+```rust
+let s = String::from("initial contents");
+```
+
+You can append to a `String` using the `push_str` method:
+
+```rust
+let mut s = String::from("foo");
+s.push_str("bar");
+```
+
+Or the `push` method to append a single character:
+
+```rust
+let mut s = String::from("lo");
+s.push('l');
+```
+
+You can concatenate `String` values using the `+` operator or the `format!` macro:
+
+```rust
+let s1 = String::from("Hello, ");
+let s2 = String::from("world!");
+let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+```
+
+Or using the `format!` macro:
+
+```rust
+let s1 = String::from("tic");
+let s2 = String::from("tac");
+let s3 = String::from("toe");
+
+let s = format!("{}-{}-{}", s1, s2, s3);
+```
+
+However, Rust does not allow you to index into a `String` to get a character. This is because a `String` is a sequence of bytes, and a character can be one or more bytes. If you need to use indices to create string slices, you can do so with a range:
+
+```rust
+let hello = "Здравствуйте";
+let s = &hello[0..4];
+```
+
+This will create a string slice containing the first 4 bytes of the string.
+
+Rust also provides methods for iterating over strings. The `chars` method returns an iterator over the `char` values in the string:
+
+```rust
+for c in "Зд".chars() {
+   println!("{}", c);
+}
+```
+
+And the `bytes` method returns an iterator over the bytes in the string:
+
+```rust
+for b in "Зд".bytes() {
+   println!("{}", b);
+}
+```
+
+Remember that valid Unicode scalar values may be made up of more than 1 byte.
+
+In summary, strings in Rust are a bit more complex than in many other languages due to their handling of Unicode. However, Rust provides a lot of functionality to handle these complex situations correctly.
+
+
+
+
+
+---------------------------------------------------------------------------------------------------------
+
+
+  Certainly! Let's use code samples to illustrate some of the concepts discussed in the provided text.
+
+### 1. Creating a New String:
+
+```rust
+// Creating a new, empty String
+let mut s = String::new();
+
+// Using to_string method
+let data = "initial contents";
+let s = data.to_string();
+
+// Using String::from function
+let s = String::from("initial contents");
+
+// Creating strings with greetings in different languages
+let hello_arabic = String::from("السلام عليكم");
+let hello_japanese = String::from("こんにちは");
+// ... (other greetings)
+```
+
+### 2. Updating a String:
+
+```rust
+// Appending a string slice to a String using push_str method
+let mut s1 = String::from("foo");
+let s2 = "bar";
+s1.push_str(s2);  // Result: foobar
+
+// Adding one character to a String value using push method
+let mut s = String::from("lo");
+s.push('l');  // Result: lol
+```
+
+### 3. Concatenation:
+
+```rust
+// Using the + operator to combine two String values into a new String value
+let s1 = String::from("Hello, ");
+let s2 = String::from("world!");
+let s3 = s1 + &s2;  // Note: s1 has been moved, and s1 can no longer be used
+
+// Using the format! macro for concatenation
+let s1 = String::from("tic");
+let s2 = String::from("tac");
+let s3 = String::from("toe");
+let s = format!("{s1}-{s2}-{s3}");  // Result: tic-tac-toe
+```
+
+### 4. Indexing into Strings:
+
+```rust
+// Attempting to use indexing syntax with a String (which is invalid)
+let s1 = String::from("hello");
+// Uncommenting the line below would result in a compilation error
+// let h = s1[0];
+```
+
+### 5. Slicing Strings:
+
+```rust
+// Slicing a string to create a string slice containing particular bytes
+let hello = "Здравствуйте";
+let s = &hello[0..4];  // Result: Зд
+```
+
+### 6. Methods for Iterating Over Strings:
+
+```rust
+// Iterating over Unicode scalar values using chars method
+for c in "Зд".chars() {
+    println!("{c}");
+}
+// Output: З\nд
+
+// Iterating over raw bytes using bytes method
+for b in "Зд".bytes() {
+    println!("{b}");
+}
+// Output: 208\n151\n208\n180
+```
+
+These code samples demonstrate various operations on strings in Rust, including creation, updating, concatenation, indexing, slicing, and iterating.
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
